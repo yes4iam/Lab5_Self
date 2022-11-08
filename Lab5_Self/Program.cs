e697105e8c1e1a3630644a7305d6380279a57b08
yesiam <yesiam@YESIAMLAPTOP>
2022-11-08 22:13:38 +0300

    Добавьте файлы проекта.

---
+++ Lab5_Self/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Self
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите элементы массива");
+            int[] array = new int[7];
+            int s = 0;
+            int k = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                s += array[i];
+                k++;
+            }
+            Console.WriteLine("Среднее арифметическое чисел:");
+            Console.WriteLine(((double)s)/k);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = ((i + j) % 2 == 0) ? 1 : 0;
+                    Console.Write("{0}", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
